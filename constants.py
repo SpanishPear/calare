8acef40cce2eddb08c25f388ee96e39127d4aad0
shrey <z5257343@unsw.edu.au>
2020-09-10 15:27:13 +1000

    Initial working version

---
+++ constants.py
@@ -0,0 +1,31 @@
+''''
+    Constants used to inform Calare's date picker
+'''
+
+from datetime import date
+from dateutil.rrule import  MO, TU, WE, TH, FR
+
+NUM_WEEKS = 10
+TUT_LENGTH = 1
+LAB_LENGTH = 2
+
+DAYS = {
+    "M" : MO,
+    "T" : TU,
+    "W" : WE,
+    "H" : TH,
+    "F" : FR
+}
+
+START_DATES = {
+    "20" : {
+        "T2": date(2020, 6, 1),
+        "T3": date(2020, 9, 14)
+    },
+    "21" : {
+        "T0": date(2021, 1, 4),
+        "T1": date(2021, 2, 8),
+        "T2": date(2021, 5, 31),
+        "T3": date(2021, 9, 13)
+    }
+}
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
-from ics import Calendar, Event
+import sys
 import os
+import time
+from ics import Calendar, Event
+from dateutil.rrule import rrule, WEEKLY
+from constants import NUM_WEEKS, TUT_LENGTH, LAB_LENGTH, DAYS, START_DATES
 
 
 def create_event(event_name, event_start, event_end):
@@ -16,7 +20,11 @@ def create_event(event_name, event_start, event_end):
     e.end = event_end
     return e
 
-def add_classes(cal):
+def add_classes(cal, offering_code, coursecode):
+    '''
+        Continuously adds classes to the ICS.
+        Uses user input of ClassCode and other settings
+    '''
     while input("Add a class? [y/n]: ").lower().startswith('y'):
         print("---- Adding Class ----")
         print("If you enter some wrong information, answer q to restart")
@@ -28,37 +36,85 @@ def add_classes(cal):
         tutor_input = input("T or AT? : ").lower()
         if tutor_input.startswith("q"):
             continue
-        tutor_bool = tutor_input.startswith('t')
-        if not tutor_bool:
-            show_tut = input("Add Tutorial and lab(y)? Or just Lab(n)? [y/n/q]: ").lower()
-            if show_tut.startswith("q"):
-                continue
-            show_bool = show_tut.startswith('y')
 
-        
+        tutor_bool = tutor_input.startswith('t')
+        show_tut = True
+
+        if not tutor_bool:
+            show_tut_input = input("Add Tutorial and lab(y)? Or just Lab(n)? [y/n/q]: ").lower()
+
+            if show_tut_input.startswith("q"):
+                continue
+
+            show_tut = show_tut_input.startswith('y')
+
+        year = offering_code[:2]
+        term = offering_code[2:]
+        start_date = START_DATES.get(year).get(term)
+
+
+        tut_start = class_code[1:3]
+        lab_start = str(int(tut_start) + TUT_LENGTH)
+        lab_end = str(int(lab_start) + LAB_LENGTH)
+        # generate the weeks
+
+        date_rule = rrule(WEEKLY, dtstart=start_date, count=NUM_WEEKS, byweekday=(DAYS[class_code[0]]))
+        for i, dt in enumerate(date_rule):
+            # midsem break - no classes
+            if i == 5:
+                continue
+
+            # add tutorial    
+            if show_tut:
+                event_name = course.upper(), class_code, "TUT"
+
+                start = str(dt.strftime("%Y-%m-%d"), tut_start+":00:00")
+                end = str(dt.strftime("%Y-%m-%d"), lab_start +":00:00")
+
+                cal.events.add(create_event(event_name, start, end))
+
+            # add lab
+            event_name = coursecode.upper(), class_code, "TUT"
+            start = str(dt.strftime("%Y-%m-%d"), lab_start+":00:00")
+            end = str(dt.strftime("%Y-%m-%d"), lab_end+":00:00")
+            cal.events.add(create_event(event_name, start, end))
+
+def get_offering():
+    ''''
+        Gets the offering from the user, and valida
+    '''
+    offering_v = input("Enter Offering,  ie 20T3: ")
+
+    # BASIC parsing because ceebs
+    while START_DATES.get(offering_v[:2]).get(offering_v[2:]) is None:
+        offering_v = input("Invalid Offering (not a 20/21 term) - try again: ")
+
+    return offering_v
+
+def print_intro():
+    '''
+        Prints the intro to the program
+    '''
+    print("Welcome to Calare\n")
+    print("This is a tool that will generate an ICS cal for your classes given classcodes and other info")
+    print("This tool is designed for Tutors doing 9 x  1 hour tutorials followed by 2 hour labs")
+    print("OR Assistant Tutors doing 2 hour Labs")
+    print("If you have other needs, this will be open source if you'd like : ^)")
 
 if __name__ == "__main__":
     c = Calendar()
-    print("Welcome to Calare\n")
-    print("This is a tool that will generate a timetable for your classes given classcodes and other info")
-    print("This tool is designed for Tutors doing 1 hour tutorials followed by 2 hour labs")
-    print("OR Assistant Tutors doing 2 hour Labs")
-    print("If you have other needs, this will be open source if you'd like : ^)")
-    
+    print_intro()
+
     name = input("Enter Name: ")
     course = input("Enter Course Code: ")
-    offering = input("Enter Offering,  ie 20T3: ")
-    c.events.add(create_event("test", "2020-10-09 12:30:00", "2020-10-09 12:45:30"))
-    
-   
-            
-        
-    
-    
-    filename = name +'-' + course + '-calare-tutor-timetable.ics'
-    if os.path.isfile('./' + filename):
-        raise Exception("Looks like the file", filename, "already exists\nPlease delete/rename it")
+    offering = get_offering()
 
+    add_classes(c, offering, course)
+
+    filename = name +'-' + course + int(time.time()) + '-calare-tutor-timetable.ics'
+    if os.path.isfile('./' + filename):
+        print("Looks like the file", filename, "already exists :(\nPlease delete/rename it")
+        sys.exit(1)
 
     with open(filename, 'w') as output_file:
-        output_file.writelines(c)    
+        output_file.writelines(c)
