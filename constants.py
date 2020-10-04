8c79e5b3d990c9b2427d1c19a3ea5a3b93d9022a
Shrey Somaiya <shrey.somaiya@gmail.com>
2020-10-04 18:08:14 +1100

    Help session feature init

--- constants.py
+++ constants.py
@@ -3,27 +3,36 @@
 '''
 
 from datetime import date
-from dateutil.rrule import  MO, TU, WE, TH, FR
+from dateutil.rrule import MO, TU, WE, TH, FR
 
 NUM_WEEKS = 10
-MIDSEMWEEK = 6
-TUT_LENGTH = 1
-LAB_LENGTH = 2
+MIDSEM_WEEK_NUMBER = 6
+TUT_LENGTH_HOURS = 1
+LAB_LENGTH_HOURS = 2
 
-DAYS = {
-    "M" : MO,
-    "T" : TU,
-    "W" : WE,
-    "H" : TH,
-    "F" : FR
+
+WEEKDAYS = {
+    "MON": MO,
+    "TUE": TU,
+    "WED": WE,
+    "THU": TH,
+    "FRI": FR
+}
+
+CLASS_DAYS = {
+    "M": MO,
+    "T": TU,
+    "W": WE,
+    "H": TH,
+    "F": FR
 }
 
 START_DATES = {
-    "20" : {
+    "20": {
         "T2": date(2020, 6, 1),
         "T3": date(2020, 9, 14)
     },
-    "21" : {
+    "21": {
         "T0": date(2021, 1, 4),
         "T1": date(2021, 2, 8),
         "T2": date(2021, 5, 31),
